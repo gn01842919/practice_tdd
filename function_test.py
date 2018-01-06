6dd84e596e13432923616e5fd9256f426f37b3f7
Kyan Zhang <gn01842919@gmail.com>
2018-01-06 22:28:26 +0800

    Update Function Test with unittest module

--- function_test.py
+++ function_test.py
@@ -1,5 +1,35 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Kyan goes to the homepage
+        self.browser.get('http://localhost:8000')
+
+        # And the title and header shows 'To-do'
+        assert 'To-Do' in self.browser.title
+
+        # He enters a todo list item
+
+        # He inputs "buy peacock feathers"
+
+        # He type Enter, and the pages refreshes and show: "1. buy peacock feathers"
+
+        # There is a input area
+
+        # He type "Make a fly"
+
+        # Now the page shows two items
+
+        # ............
+
+
+if __name__ == '__main__':
+    unittest.main()
