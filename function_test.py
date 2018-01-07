5d3c5dd6d2d58cb83e1cd8c91135116f4c3ca7bb
Kyan Zhang <gn01842919@gmail.com>
2018-01-07 10:06:51 +0800

    Functional test now checks we can input a to-do item

--- function_test.py
+++ function_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,10 +16,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # And the title and header shows 'To-do'
         assert 'To-Do' in self.browser.title
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
         # He enters a todo list item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He inputs "buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy peacock feathers' for row in rows)
+        )
 
         # He type Enter, and the pages refreshes and show: "1. buy peacock feathers"
 
