db149ee42adc3be2a7e0fea843513139df17a846
Kyan Zhang <gn01842919@gmail.com>
2018-01-07 13:26:55 +0800

    Refactor function_test.py and switch from selenium 3 to selenium 2, and use PhantomJS instead of Firefox

--- function_test.py
+++ function_test.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-import time
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        # self.browser = webdriver.Firefox()
+        self.browser = webdriver.PhantomJS(executable_path='C:\\Program Files\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe')
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        print(rows)
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -20,7 +26,6 @@ class NewVisitorTest(unittest.TestCase):
         assert 'To-Do' in self.browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
 
         # He enters a todo list item
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -35,12 +40,17 @@ class NewVisitorTest(unittest.TestCase):
         # He type Enter, and the pages refreshes and show: "1. buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is a input area
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: may a fly')
+
+
 
         # He type "Make a fly"
 
