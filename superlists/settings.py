11e8ed283a7914906e68c0ad8b42bc80cb8e93a8
Kyan Zhang <gn01842919@gmail.com>
2018-01-12 21:00:15 +0800

    Refactor functional tests

--- superlists/settings.py
+++ superlists/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'database', 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # Not worked on my machine!!
+        # 'NAME': os.path.join(BASE_DIR, '..', 'database', 'db.sqlite3'),
+        # Originally:
+        # 'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
